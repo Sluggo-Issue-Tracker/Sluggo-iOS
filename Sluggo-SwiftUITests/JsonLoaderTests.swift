e30ca35b65ff0bc06d248aa5314570c6fb58f256
Andrew Gavgavian <agavgavi@ucsc.edu>
2021-07-16 21:54:28 -0700

    Built a mock for JsonLoader to return a different object based on the contents of the URLrequest passed

--- Sluggo-SwiftUITests/JsonLoaderTests.swift
+++ Sluggo-SwiftUITests/JsonLoaderTests.swift
@@ -10,17 +10,52 @@ import XCTest
 
 class MockJsonLoader: BaseLoader, CanNetworkRequest {
     func executeCodableRequest<T: Codable>(request: URLRequest) async -> Result<T, Error> {
-        return .failure(RESTException.failedRequest(message: "This is a Mock Error"))
+        let path = request.url
+        let ticket = TicketRecord(id: 1,
+                                  ticketNumber: 1,
+                                  tagList: [],
+                                  objectUuid: UUID(),
+                                  assignedUser: nil,
+                                  status: nil,
+                                  title: "Test Ticket",
+                                  description: nil,
+                                  dueDate: nil,
+                                  created: Date(),
+                                  activated: nil,
+                                  deactivated: nil)
+        let tags = TagRecord(id: 1,
+                             teamId: 1,
+                             objectUuid: UUID(),
+                             title: "Test Tag",
+                             created: Date(),
+                             activated: nil,
+                             deactivated: nil)
+        let status = StatusRecord(id: 1,
+                                  objectUuid: UUID(),
+                                  title: "Test Status",
+                                  color: "#FFFFFFFF",
+                                  created: Date(),
+                                  activated: nil,
+                                  deactivated: nil)
+        if let urlString = path?.absoluteString {
+            switch urlString {
+            case _ where urlString.contains("/tickets/"):
+                return .success(ticket as! T)
+            case _ where urlString.contains("/tags/"):
+                return .success(tags as! T)
+            case _ where urlString.contains("/statuses/"):
+                return .success(status as! T)
+            default:
+                break
+            }
+        }
+        return .failure(RESTException.failedRequest(message: "Not an ticket, tag, or status url"))
     }
     
     func executeEmptyRequest(request: URLRequest) async -> Result<Void, Error> {
         return .success(())
     }
-    
-    
 }
-
-
 
 class JsonLoaderTests: XCTestCase {
 
