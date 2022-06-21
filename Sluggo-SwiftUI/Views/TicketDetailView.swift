e0ea962ddfc32bd531adb2c33965667e21810db5
Troy Ebert <troy.ebert@outlook.com>
2022-06-21 11:08:10 -0700

    Created edit view modally, can be dismissed through button or dragging view

--- Sluggo-SwiftUI/Views/TicketDetailView.swift
+++ Sluggo-SwiftUI/Views/TicketDetailView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct TicketDetail: View {
     
     @State var ticket: TicketRecord
+    @State var showModalView = false
     
     var body: some View {
 
@@ -54,6 +55,64 @@ struct TicketDetail: View {
             
         }
         .navigationBarTitleDisplayMode(.inline)
-        .navigationBarItems(trailing: Button("Edit") {})
+        .navigationBarItems(trailing: Button(action: {
+            self.showModalView.toggle()
+        }) {
+            Text("Edit")
+        }
+            .sheet(isPresented: $showModalView) {
+                TicketEditDetail(ticket: ticket, showModalView: $showModalView)
+                
+            })
+    }
+}
+
+struct TicketEditDetail: View {
+    
+    @State var ticket: TicketRecord
+    @Binding var showModalView: Bool
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Title")) {
+                    Text(ticket.title)
+                }
+                Section(header: Text("Assigned User")) {
+                    Text("\(ticket.assignedUser?.getTitle() ?? "")")
+                }
+                Section(header: Text("Status")) {
+                    Text("\(ticket.status?.getTitle() ?? "")")
+                }
+                Section(header: Text("Tags")) {
+                    if(ticket.tagList.isEmpty) {
+                        Text("")
+                    }
+                    else {
+                        ForEach(ticket.tagList) { tag in
+                            Text(tag.title)
+                        }
+                    }
+                }
+                Section(header: Text("Date Due")) {
+                    if(ticket.dueDate == nil) {
+                        Text("")
+                    }
+                    else {
+                        Text(ticket.dueDate ?? Date(), style: .date)
+                    }
+                }
+                Section(header: Text("Description")) {
+                    VStack(spacing: 0) {
+                        Spacer()
+                        Text("\(ticket.description ?? "")")
+                            .frame(minHeight: 100, alignment: .topLeading)
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(leading: Button("Cancel") {self.showModalView.toggle()}, trailing: Button("Done") {})
+        }
+        
     }
 }
