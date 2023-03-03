157ebb9c082211a71858ba5f83fa548843e3d55b
Andrew Gavgavian <andrewgee7@gmail.com>
2023-03-02 20:56:11 -0800

    Cleaned up errors, removed tests.

--- Sluggo-SwiftUI/Models/Exceptions.swift
+++ Sluggo-SwiftUI/Models/Exceptions.swift
@@ -16,47 +16,6 @@ enum Exception: Error {
     case runtimeError(message: String)
 }
 
-extension UIAlertController {
-    static func errorController(error: Error, handler: ((UIAlertAction) -> Void)?) -> UIAlertController {
-        // Setup message
-        var message: String?
-        
-        if let failedRequestError = error as? RESTException {
-            switch failedRequestError {
-            case .failedRequest(let errMsg):
-                message = errMsg
-            }
-        }
-        
-        if let exceptionError = error as? Exception {
-            switch exceptionError {
-            case .runtimeError(let errMsg):
-                message = errMsg
-            }
-        }
-        
-        let alert = UIAlertController(title: "Error",
-                                      message: message ?? "Error message not provided", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: handler))
-        
-        return alert
-    }
-    
-    static func errorController(error: Error) -> UIAlertController {
-        return self.errorController(error: error, handler: nil)
-    }
-    
-    static func createAndPresentError(view: UIViewController, error: Error, completion: ((UIAlertAction) -> Void)?) {
-        let alertController = UIAlertController.errorController(error: error, handler: completion)
-        
-        view.present(alertController, animated: true, completion: nil)
-    }
-    
-    static func createAndPresentError(view: UIViewController, error: Error) {
-        UIAlertController.createAndPresentError(view: view, error: error, completion: nil)
-    }
-}
-
 public enum LogLevel: Int{
     case debug
     case info
